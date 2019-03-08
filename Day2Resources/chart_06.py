7368b218bcf065c66650e6677705a33c20049c5d
kwseow <seow_khee_wei@yahoo.com.sg>
2019-03-08 08:53:23 +0800

    Add files via upload

---
+++ Day2Resources/chart_06.py
@@ -0,0 +1,25 @@
+import csv
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+
+def format_minutes(value, pos):
+	return '{}m'.format(int(value))
+
+def format_dollars(value, pos):
+	return '${}'.format(value)
+
+# read data from csv
+with open('scatter.csv') as fp:
+	reader = csv.reader(fp)
+	data = list(reader)
+
+data_x = [float(x) for x, y, in data]
+data_y = [float(y) for x, y in data]
+plt.scatter(data_x, data_y)
+
+plt.gca().xaxis.set_major_formatter(FuncFormatter(format_minutes))
+plt.xlabel('Time in website')
+plt.gca().yaxis.set_major_formatter(FuncFormatter(format_dollars))
+plt.ylabel('Spending')
+
+plt.show()
